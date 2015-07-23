cd52f1ebb091a5e83cdd282863c4a11d37431f52
bigeasy77 <amw044@amw044-03.ds.mot.com>
2015-07-23 18:31:49 +0200

    Initial commit

---
+++ app/src/main/java/com/oxyence/orientational/core/Recorder.java
@@ -0,0 +1,23 @@
+package com.oxyence.orientational.core;
+
+import java.util.Vector;
+
+/**
+ * Created by amw044 on 7/14/2015.
+ */
+
+class Sample {
+
+}
+
+public class Recorder {
+
+    public void start() {
+
+    }
+
+    public Vector getSeries() {
+        return new Vector();
+    }
+
+}
